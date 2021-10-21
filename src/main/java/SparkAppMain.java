052d13be312b35ca7140078f1e811ca1f6b0531a
Shivansh Anand <shivansh586@gmail.com>
2021-10-21 18:12:04 +0530

    added dataframe

--- src/main/java/SparkAppMain.java
+++ src/main/java/SparkAppMain.java
@@ -1,109 +1,82 @@
-import org.apache.commons.collections.map.LinkedMap;
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaDoubleRDD;
-import org.apache.spark.api.java.JavaPairRDD;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.mllib.linalg.*;
-import org.apache.spark.mllib.clustering.*;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import org.apache.spark.mllib.linalg.Vector;
-import org.apache.spark.rdd.RDD;
-import scala.Tuple2;
-import shapeless.Tuple;
 
-//import javax.swing.*;
-import java.io.IOException;
-import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-//import java.util.stream.Collectors;
+// $example on$
+import org.apache.spark.ml.clustering.KMeansModel;
+import org.apache.spark.ml.clustering.KMeans;
+import org.apache.spark.ml.evaluation.ClusteringEvaluator;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.ml.linalg.Vector;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+// $example off$
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.*;
 
-class Pair<T, V> {
-    T key;
-    V value;
-    Pair(T key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-    T getKey() {
-        return this.key;
-    }
-    V getValue() {
-        return this.value;
-    }
-}
+import java.util.Arrays;
 
+
+/**
+ * An example demonstrating k-means clustering.
+ * Run with
+ * <pre>
+ * bin/run-example ml.JavaKMeansExample
+ * </pre>
+ */
 public class SparkAppMain {
-    static <K,V extends Comparable<? super V>>
-    List<Map.Entry<K, V>> entriesSortedByValues(Map<K,V> map) {
 
-        List<Map.Entry<K,V>> sortedEntries = new ArrayList<Map.Entry<K,V>>(map.entrySet());
+  public static void main(String[] args) {
+    // Create a SparkSession.
+    SparkSession spark = SparkSession
+      .builder()
+      .appName("JavaKMeansExample")
+      .getOrCreate();
 
-        Collections.sort(sortedEntries,
-                new Comparator<Map.Entry<K,V>>() {
-                    @Override
-                    public int compare(Map.Entry<K,V> e1, Map.Entry<K,V> e2) {
-                        return e2.getValue().compareTo(e1.getValue());
-                    }
-                }
-        );
+    // $example on$
+    // Loads data.
+    Dataset<Row> dataset = spark.read().option("header", false).option("inferSchema", true).csv("hdfs://localhost:9000/user/hdoop/data/kddcup.data");
+    dataset = dataset.drop("_c1", "_c2", "_c3");
 
-        return sortedEntries;
+    String[] cols = dataset.columns();
+    cols = Arrays.copyOf(cols, cols.length-1);
+    System.out.println(Arrays.toString(cols));
+
+    VectorAssembler assembler = new VectorAssembler()
+            .setInputCols(cols)
+            .setOutputCol("features");
+
+    dataset = assembler.transform(dataset);
+
+    for(int i=2; i<150; i++){
+      KMeans kmeans = new KMeans().setK(i).setSeed(1L);
+      KMeansModel model = kmeans.fit(dataset);
+
+      // Make predictions
+      Dataset<Row> predictions = model.transform(dataset);
+
+      // Evaluate clustering by computing Silhouette score
+      ClusteringEvaluator evaluator = new ClusteringEvaluator();
+
+      double silhouette = evaluator.evaluate(predictions);
+      System.out.println("Silhouette with squared euclidean distance = " + silhouette + " | Clusters: " + i);
+      // $example off$
+      // Trains a k-means model.
     }
-    public static <A, B> List<Pair<A, B>> zipJava8(List<A> as, List<B> bs) {
-        return IntStream.range(0, Math.min(as.size(), bs.size()))
-                .mapToObj(i -> new Pair<>(as.get(i), bs.get(i)))
-                .collect(Collectors.toList());
-    }
-    public static double distanceToCentroid(Vector datum, KMeansModel model) {
-        int cluster = model.predict(datum);
-        Vector centroid = model.clusterCenters()[cluster];
-        return Math.sqrt(Vectors.sqdist(centroid, datum));
-    }
-    public static double clusteringScore(RDD<Vector> data, int k) {
-        KMeans kMeans = new KMeans();
-        kMeans.setK(k);
-        KMeansModel model = kMeans.run(data);
-        JavaDoubleRDD doubleRDD = data.toJavaRDD().mapToDouble(datum -> distanceToCentroid(datum, model));
-        return doubleRDD.stats().mean();
-    }
-    public static void main(String[] args) throws IOException {
-        SparkConf sparkConf = new SparkConf().setAppName("Anomaly Detection");
-        JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
-        JavaRDD<String> javaRDD = sparkContext.textFile("hdfs://localhost:9000/user/hdoop/data/kddcup.data");
-        JavaPairRDD<String, Vector> labelsAndData = javaRDD.mapToPair(line -> {
-           List<String> ll = Arrays.asList(line.split(","));
-           List<String> list = new ArrayList<String>(ll);
-           list.remove(1);
-           list.remove(1);
-           list.remove(1);
-           String label = list.remove(list.size()-1);
-           double vectorArray[] = new double[list.size()];
-           for(int l=0; l<list.size(); ++l) {
-               vectorArray[l] = Double.parseDouble(list.get(l));
-           }
-           Vector dv = Vectors.dense(vectorArray);
-           return new Tuple2<String, Vector>(label, dv);
-        });
-        JavaRDD<Vector> data =  labelsAndData.values().cache();
-        KMeans kmeans = new KMeans();
-        KMeansModel model = kmeans.run(data.rdd());
-        System.out.println("------------------------------------------");
-        Map<Tuple2<Integer, String>, Long> clusterLabelCountRDD = labelsAndData.mapToPair(t -> {
-            String label = t._1;
-            Vector datum = t._2;
-            int cluster = model.predict(datum);
-            return new Tuple2<Integer, String>(cluster, label);
-        }).countByValue();
-
-        for(Tuple2<Integer, String> key: clusterLabelCountRDD.keySet()){
-            System.out.println(key.toString() + " : " + clusterLabelCountRDD.get(key));
-        }
-
-        for(int i=5; i<=40; i+=5){
-            System.out.println(i + "\t" + clusteringScore(JavaRDD.toRDD(data), i));
-        }
-
-    }
+    spark.stop();
+  }
 }
